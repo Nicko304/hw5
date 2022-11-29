95435e1cbc3b625bf3ee41838ba6f95914db71ea
Nicholas Schumacher <naschuma@usc.edu>
2022-11-29 02:51:23 +0000

    Adding pdfs and final submission with schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,6 +19,21 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t shift_counter[],
+    size_t row,
+    size_t col);
+
+bool isValid(
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t row,
+    size_t col);
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -35,9 +50,50 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    
+    for(size_t i = 0; i < avail.size(); i++){ // avail.size() = n days
+      std::vector<Worker_T> temp(dailyNeed, INVALID_ID);
+      sched.push_back(temp);
+    }
+    size_t* shift_counter = new size_t[avail[0].size()];
+    // Store shift counts of k possible workers as the size, provided from avail[0].size()
+    for(size_t i = 0; i < avail[0].size(); i++){
+      shift_counter[i] = 0; // Make every worker start with 0 shifts given
+    }
+    bool foundSchedule = schedule_helper(avail, dailyNeed, maxShifts, sched, shift_counter, 0, 0);
+    delete[] shift_counter;
+    return foundSchedule;
 }
 
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t shift_counter[],
+    size_t row,
+    size_t col){
+      
+    if(row == avail.size()){
+        return true; // avail.size() = n days
+    }
+    for(size_t i = 0; i < avail[row].size(); i++){ // Iterate through all workers for a given day
+        if( avail[row][i] == 1 && shift_counter[i] + 1 <= maxShifts){ 
+            // If worker is available to work and if adding a shift stays within max shift constraint
+            sched[row][col] = i; // Put worker id in the schedule cell (will be overridden on next iteration if schedule doesn't work)
+            shift_counter[i]++; // Add the shift to the worker's shift counter
+            if(col == dailyNeed - 1){
+                if(schedule_helper(avail, dailyNeed, maxShifts, sched, shift_counter, row + 1, 0) == true){
+                    return true;
+                }
+            }
+            else{
+                if(schedule_helper(avail, dailyNeed, maxShifts, sched, shift_counter, row, col + 1) == true){
+                    return true;
+                }
+            }
+            shift_counter[i]--; // Remove worker's shift if sched ended up being invalid
+        }
+    }
+    return false;
+}
